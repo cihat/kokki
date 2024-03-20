d0c2c46d7d41321b99e03a10493de51d521eb28d
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-03-20 18:38:46 +0300

    Add some types for models

---
+++ backend/src/types/index.js
@@ -0,0 +1,8 @@
+const dietTypes = ["vegan", "vegetarian", "pescatarian", "omnivore", "flexitarian"]
+
+const ingredientTypes = ["fruit", "vegetable", "grain", "protein", "dairy", "other"]
+
+module.exports = {
+  dietTypes,
+  ingredientTypes
+}
