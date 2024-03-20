a17cd3413e01d5fc77f0337128a10091b3441b97
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-03-20 18:34:20 +0300

    Fix dabase connection warning for mongoose new version

--- backend/src/database-connection.js
+++ backend/src/database-connection.js
@@ -3,13 +3,10 @@ const mongoose = require('mongoose')
 
 const connectionString = process.env.MONGODB_CONNECTION_STRING || 'mongodb://localhost'
 
-mongoose.set('debug', true)
+mongoose.set('debug', false)
 
 mongoose
-  .connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(connectionString)
   .then(() => console.log('Database connection established.'))
   .catch(console.log)
 
