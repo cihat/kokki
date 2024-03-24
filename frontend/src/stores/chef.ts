5c5a6f203844936f00d539e23f0e08ea65cfe806
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-03-24 18:59:08 +0300

    Add chef store

---
+++ frontend/src/stores/chef.ts
@@ -0,0 +1,33 @@
+import { reactive } from 'vue'
+import { defineStore } from 'pinia'
+import axios from 'axios'
+
+const useChefStore = defineStore('chef', () => {
+  const state = reactive({
+    chef: {}
+  })
+
+  const fetchChef = async () => {
+    try {
+      const { data: { chef: fetchedChef } } = await axios.post("/food/suggestion", {
+        "chefId": "65fae55f6e003ed87048c8d9",
+        "similarity": 0.4
+      });
+
+      Object.assign(state.chef, fetchedChef)
+    } catch (error) {
+      console.error('Error fetching users:', error)
+    }
+  }
+
+  onMounted(async () => {
+    await fetchChef()
+  })
+
+  return {
+    state,
+    fetchChef
+  }
+})
+
+export default useChefStore;
