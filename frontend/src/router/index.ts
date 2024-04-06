61de2ea40c93c5112ea616c10efd9f95f270295b
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-04-07 02:56:38 +0300

    WIP: Check `isLoggedIn` for dynamic routing
    - Wrapper a function beacuse conditional view

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,26 +2,50 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Kitchen from '../views/Kitchen.vue'
 import Register from '@/views/Register.vue'
 import Login from '@/views/Login.vue'
+import Home from '@/views/Home.vue'
+//! Uncomment the following line to use the account store
+// import useAccountStore from '@/stores/account'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/kitchen',
-      name: 'home',
-      component: Kitchen
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: Register
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },
-  ]
-})
+export default function routerInit() {
+  // const { isLoggedIn } = useAccountStore()
 
-export default router
+  const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+      {
+        path: '/',
+        name: 'home',
+        component: Home,
+      },
+      {
+        path: '/register',
+        name: 'register',
+        component: Register,
+        // beforeEnter(to, from, next) {
+        //   if (isLoggedIn) next({ name: 'kitchen' })
+        //   else next()
+        // }
+      },
+      {
+        path: '/login',
+        name: 'login',
+        component: Login,
+        // beforeEnter(to, from, next) {
+        //   if (isLoggedIn) next({ name: 'kitchen' })
+        //   else next()
+        // }
+      },
+      {
+        path: '/kitchen',
+        name: 'kitchen',
+        component: Kitchen,
+        // beforeEnter(to, from, next) {
+        //   if (!isLoggedIn) next({ name: 'login' })
+        //   else next()
+        // }
+      }
+    ]
+  })
+
+  return router
+}
