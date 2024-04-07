2592c3147ed92bd7809df1dbcc6baa6ff11cfd47
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-04-08 00:47:24 +0300

    Fix warning message due to unnecessary prop granting

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
 const route = useRoute();
-const selectedKeys = computed(() => [route.name])
 const layout = computed(() => `${route.meta.layout}-layout` || 'default-layout')
 </script>
 
 <template>
   <a-layout theme="light">
-    <component :is="layout" :selectedKeys="selectedKeys" />
+    <component :is="layout" />
   </a-layout>
 </template>
