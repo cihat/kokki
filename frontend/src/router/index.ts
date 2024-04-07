2a911fa1bd9a740770d9abbffdfd788ff454c56e
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-04-07 16:40:00 +0300

    WIP: Comment in router because not working corretly

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,22 +3,21 @@ import Kitchen from '../views/Kitchen.vue'
 import Register from '@/views/Register.vue'
 import Login from '@/views/Login.vue'
 import Home from '@/views/Home.vue'
-import useAccountStore from '@/stores/account'
+// import useAccountStore from '@/stores/account'
 
 export default function routerInit() {
-  const accountStore = useAccountStore() as any;
-  const isLoggedIn = computed(() => accountStore.isLoggedIn);
-  console.log('isLoggedIn', isLoggedIn);
+  // const accountStore = useAccountStore() as any;
+  // const isLoggedIn = computed(() => accountStore.isLoggedIn);
 
-  const routeAuthGuard = (to, from, next) => {
-    if (!isLoggedIn) next({ name: 'login' })
-    else next()
-  }
+  // const routeAuthGuard = (to, from, next) => {
+  //   if (!isLoggedIn) next({ name: 'login' })
+  //   else next()
+  // }
 
-  const redirectIfLoggedIn = (to, from, next) => {
-    if (isLoggedIn) next({ name: 'kitchen' })
-    else next()
-  }
+  // const redirectIfLoggedIn = (to, from, next) => {
+  //   if (isLoggedIn) next({ name: 'kitchen' })
+  //   else next()
+  // }
 
   const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
