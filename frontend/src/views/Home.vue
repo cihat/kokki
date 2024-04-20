2895a27b9ed0304efecb6ba786dc367b1754c8bf
Cihat SALİK <57585087+cihat@users.noreply.github.com>
2024-04-21 02:01:10 +0300

    Add landing page and generated by ai landing page photo

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,17 +1,40 @@
 <script setup lang="ts">
+import useAccountStore from '@/stores/account';
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
+const { isLoggedIn } = useAccountStore();
+
+const infoMessage = () => {
+  if (!isLoggedIn) {
+    router.push('/login');
+  } else {
+    router.push('/kitchen');
+  }
+}
 </script>
 
 <template>
-  <h1 class="big-title">
-    <router-link to="/kitchen">Kitchen</router-link>
-  </h1>
+  <a-result title="Kitchen Dilemmas: A Culinary Crossroads" sub-title="Navigating Indecision with Ingredients at Hand">
+    <template #icon>
+      <a-image :preview="false" previewMask="false" src="../../public/landing/landing-image.jpeg" />
+    </template>
+    <template #extra>
+      <h1 class="big-title" @click="infoMessage">👉🏻 Kitchen 🍽️</h1>
+    </template>
+  </a-result>
 </template>
 
 <style scoped lang="scss">
 .big-title {
   font-size: 3em;
   margin-top: 2em;
-  margin-bottom: 2em;
+  text-decoration: underline;
+  cursor: pointer;
+  font-weight: bold;
+  color: #333333;
+  text-align: center;
+  text-transform: uppercase;
+  letter-spacing: 2px;
 }
 </style>
